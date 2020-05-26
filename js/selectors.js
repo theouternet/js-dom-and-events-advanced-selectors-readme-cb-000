f3235c3b1d5db5314e639c10b63dc571c3013719
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-26 21:43:48 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+function lastImageSelector(){
+  return $('li img:last');
+}
+
+function divSelector(){
+  return $('div');
+}
+
+function firstListItem(){
+  return $('ul li:first-child');
+}
